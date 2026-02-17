4359eb3bdc5ff5b927026e0d8cf8caae059c4a29
bkaras-g <bkaras-g@student.42.fr>
2026-02-17 11:10:47 +0100

    add Dog class

---
+++ ex00/Dog.cpp
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Dog.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bkaras-g <bkaras-g@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/17 10:43:23 by bkaras-g          #+#    #+#             */
+/*   Updated: 2026/02/17 11:06:52 by bkaras-g         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Dog.hpp"
+#include <iostream>
+
+Dog::Dog(void) : _type("Dog")
+{
+	std::cout << "Dog default constructor called" << std::endl;
+}
+
+Dog::Dog(const Dog& copy)
+{
+	std::cout << "Dog copy constructor called" << std::endl;
+	*this = copy;
+}
+
+Dog::~Dog(void)
+{
+	std::cout << "Dog destructor called" << std::endl;
+}
+
+Dog& Dog::operator=(const Dog& copy)
+{
+	std::cout << "Dog assignment operator called" << std::endl;
+	if (this != &copy) {
+		*this = copy;
+	}
+	return (*this);
+}
+
+void Dog::makeSound()
+{
+	std::cout << "WOUF WOUF" << std::endl;
+}
---
+++ ex00/Dog.hpp
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Dog.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bkaras-g <bkaras-g@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/17 10:49:15 by bkaras-g          #+#    #+#             */
+/*   Updated: 2026/02/17 11:02:37 by bkaras-g         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DOG_HPP
+#define DOG_HPP
+
+#include <string>
+#include "Animal.hpp"
+
+class Dog : public Animal {
+public:
+	// ---------- Constructors / Destructor ----------
+
+	Dog(void);
+	Dog(const Dog& copy);
+	~Dog(void);
+
+	// ---------- Overloading Operators Methods -------
+
+	Dog& operator=(const Dog& copy);
+
+	// ---------- Getter and Setter Methods ------------
+
+	std::string getType();
+
+	// ---------- Member Methods -----------------------
+
+	virtual void makeSound();
+
+protected:
+	// ---------- Protected Data Members ---------------------
+	std::string _type;
+
+private:
+	// ---------- Private Data members -------------------------
+};
+
+#endif // DOG_HPP
