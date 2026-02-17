b0ce67a656e013302a50f7c4dd2f4e8742e5e305
bkaras-g <bkaras-g@student.42.fr>
2026-02-17 15:56:54 +0100

    add animals array in main()

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bkaras-g <bkaras-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/17 11:15:57 by bkaras-g          #+#    #+#             */
-/*   Updated: 2026/02/17 14:56:09 by bkaras-g         ###   ########.fr       */
+/*   Updated: 2026/02/17 15:55:44 by bkaras-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,50 +16,26 @@
 #include "WrongAnimal.hpp"
 #include "WrongCat.hpp"
 
+
+/*
+*In your main function, create and fill an array of Animal objects. Half of it will
+be Dog objects and the other half will be Cat objects. At the end of your program
+execution, loop over this array and delete every Animal. You must delete directly dogs
+and cats as Animals. The appropriate destructors must be called in the expected order.
+*/
 int main(void)
 {
-	const Animal* meta = new Animal();
-	const Animal* j = new Dog();
-	const Animal* i = new Cat();
-	const Animal* copy_i(i);
-	std::cout << j->getType() << " " << std::endl;
-	std::cout << i->getType() << " " << std::endl;
-	std::cout << "Expected cat sound:";
-	i->makeSound(); //will output the cat sound!
-	std::cout << "Expected cat sound:";
-	copy_i->makeSound();
-	std::cout << "Expected dog sound:";
-	j->makeSound();
-	delete j;
-	j = i;
-	std::cout << "Expected cat sound:";
-	j->makeSound();
-	std::cout << "Expected Animal sound:";
-	meta->makeSound();
-	delete i;
-	delete meta;
+	Animal *animals[10];
+	for (int i = 0; i < 10; i++)
+	{
+		if (i < 5)
+			animals[i] = new Cat();
+		else
+			animals[i] = new Dog();
+	}
 	std::cout << '\n';
-
-	Dog dog;
-	Cat cat;
-	Animal &rAnimalDog = dog;
-	Animal &rAnimalCat = cat;
-	std::cout << "dog is a " << dog.getType() << " " << std::endl;
-	std::cout << "cat is a " << cat.getType() << " " << std::endl;
-	std::cout << "rAnimalDog is a " << rAnimalDog.getType() << " " << std::endl;
-	std::cout << "rAnimalCat is a " << rAnimalCat.getType() << " " << std::endl;
-	std::cout << "Expected cat sound:";
-	rAnimalCat.makeSound();
-	std::cout << "Expected dog sound:";
-	rAnimalDog.makeSound();
-	std::cout << '\n';
-
-	WrongCat wrong_cat;
-	WrongAnimal &rWrongAnimalCat = wrong_cat;
-	std::cout << "wrong_cat is a " << wrong_cat.getType() << " " << std::endl;
-	std::cout << "rWrongAnimalCat is a " << rWrongAnimalCat.getType() << " " << std::endl;
-	std::cout << "Expected WrongAnimal sound:";
-	rWrongAnimalCat.makeSound();
-	std::cout << '\n';
-	return (0);
+	for (int i = 0; i < 10; i++)
+	{
+		delete animals[i];
+	}
 }
