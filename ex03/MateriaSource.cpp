98128ae87c9403c163274f675b44a77ff1f02d44
bkaras-g <bkaras-g@student.42.fr>
2026-02-26 18:45:03 +0100

    implement learnMateria(). Need to check if it's the correct way

--- ex03/MateriaSource.cpp
+++ ex03/MateriaSource.cpp
@@ -6,7 +6,7 @@
 /*   By: bkaras-g <bkaras-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/26 18:06:48 by bkaras-g          #+#    #+#             */
-/*   Updated: 2026/02/26 18:26:45 by bkaras-g         ###   ########.fr       */
+/*   Updated: 2026/02/26 18:43:35 by bkaras-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,4 +47,24 @@ MateriaSource& MateriaSource::operator=(const MateriaSource& copy)
 		}
 	}
 	return (*this);
+}
+
+/*
+Copies the Materia passed as a parameter and stores it in memory so it can be cloned
+later. Like the Character, the MateriaSource can know at most 4 Materias. They
+are not necessarily unique.
+*/
+void MateriaSource::learnMateria(AMateria* tolearn)
+{
+	int	slot = 0;
+
+	while(this->_MateriaSourceTab[slot] && slot < 4)
+		slot++;
+	if (slot == 4)
+	{
+		std::cout << "MateriaSource tab is full!" << std::endl;
+		return ;
+	}
+	else
+		this->_MateriaSourceTab[slot] = tolearn;
 }
